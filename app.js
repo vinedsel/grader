1e718a68a0ed2420ba693302e6f9650b3be9646e
Raymond Gregory <vinedsel@gmail.com>
2017-06-03 01:22:18 -0500

    Created grading engine

---
+++ app.js
@@ -0,0 +1,13 @@
+let grade;
+
+if (grade < 100) {
+  alert("A");
+} else if (grade < 90) {
+  alert("B");
+} else if (grade < 80) {
+  alert("C");
+} else if (grade < 70) {
+  alert("D");
+} else if (grade < 60) {
+  alert("F");
+}
