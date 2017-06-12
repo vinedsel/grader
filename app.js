a6187942f2c218bad47dfbe6b90e60d37bc1162b
Raymond Gregory <vinedsel@gmail.com>
2017-06-12 17:26:02 -0500

    modified

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
-let grade;
+function grader() {
 
-if (grade <= 100) {
-  alert("A");
-} else if (grade <= 90) {
-  alert("B");
-} else if (grade <= 80) {
-  alert("C");
-} else if (grade <= 70) {
-  alert("D");
-} else if (grade <= 60) {
-  alert("F");
-}
+  let grade;
+
+  if (grade <= 100) {
+    alert("A");
+  } else if (grade <= 90) {
+    alert("B");
+  } else if (grade <= 80) {
+    alert("C");
+  } else if (grade <= 70) {
+    alert("D");
+  } else if (grade <= 60) {
+    alert("F");
+  }
+};
+
+document.getElementById("grdButton").addEventListener("click", grader() {
+
+});
