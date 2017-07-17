a632bfc64b988542180f4734105ea0ca2a2972c8
Raymond Gregory <vinedsel@gmail.com>
2017-07-17 13:37:17 -0500

    modded files

--- app.js
+++ app.js
@@ -1,24 +1,49 @@
 var total = 0;
-
 var sum = 0;
 
 function gradeConv() {
 
-  var grade = parseInt(document.getElementById(“gradeValue”).value);
+  var grade1 = parseInt(document.getElementById(“grade1”).value);
+  document.getElementById('letter1').innerHTML = returnGrade(grade1);
 
-  if (grade >=90 && grade <= 100) {
-    document.("A");
-  } else if (grade >= 80 && grade <= 90) {
-    alert("B");
-  } else if (grade >= 70 && grade <= 80) {
-    alert("C");
-  } else if (grade >= 60 && grade <= 70) {
-    alert("D");
-  } else if (grade < 60) {
-    alert("F");
-  }
+  var grade2 = parseInt(document.getElementById(“grade2”).value);
+  document.getElementById('letter2').innerHTML = returnGrade(grade2);
+
+  var grade3 = parseInt(document.getElementById(“grade3”).value);
+  document.getElementById('letter3').innerHTML = returnGrade(grade3);
+
 };
 
-document.getElementById("grdButton");
-document.getElementById("gradeValue");
-addEventListener("click", gradeConv());
+function returnGrade(input) {
+  if (typeof input == "number") {
+    if (input >=90 && input <= 100) {
+      sum += input;
+      total++;
+      return "A";
+    } else if (input >= 80 && input <= 90) {
+      sum += input;
+      total++;
+      return "B";
+    } else if (input >= 70 && input <= 80) {
+      sum += input;
+      total++;
+      return "C";
+    } else if (input >= 60 && input <= 70) {
+      sum += input;
+      total++;
+      return "D";
+    } else if (input < 60) {
+      sum += input;
+      total++;
+      return "F";
+    }
+  }
+
+};
+
+function mean() {
+  gradeConv();
+  let averageGrade = sum / total;
+  document.getElementById('mean').innerHTML = "The average grade is " + averageGrade;
+
+};
